5498f4b7ddf77a26a1dee62db665a980df18ae2e
qzy7101 <m2081152264@163.com>
2019-05-30 21:07:57 +0800

    5-30 21:07

--- src/main/java/com/qf/controller/AmdinsController.java
+++ src/main/java/com/qf/controller/AmdinsController.java
@@ -1,20 +1,23 @@
 package com.qf.controller;
 
+import com.qf.pojo.Admininfo;
 import com.qf.pojo.Admins;
 import com.qf.service.AdminsService;
+import com.qf.service.AdminsinfoService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 public class AmdinsController {
 
     @Resource
     private AdminsService adminsService;
+
+    @Resource
+    private AdminsinfoService adminsinfoService;
 
     //登陆
     @RequestMapping(value = "/adminlogin",method = RequestMethod.POST)
@@ -28,7 +31,26 @@ public class AmdinsController {
         }else {
             return 2;
         }
-
     }
+
+    //根据admin查询个人信息
+    @RequestMapping(value = "/gexx")
+    @ResponseBody
+    public List<Admininfo> gexx(@RequestParam String admin){
+
+        return adminsinfoService.login(admin);
+    }
+
+    //修改个人信息
+    @RequestMapping(value = "/adminupdata")
+    @ResponseBody
+    public int upadmininfo(@RequestBody Admininfo admininfo){
+
+        System.out.println(admininfo.toString());
+        adminsinfoService.upadmininfo(admininfo);
+
+        return 1;
+    }
+
 
 }
---
+++ src/main/java/com/qf/mapper/AdminsinfoMapper.java
@@ -0,0 +1,22 @@
+package com.qf.mapper;
+
+import com.qf.pojo.Admininfo;
+import com.qf.pojo.Admins;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface AdminsinfoMapper {
+
+    //(通过admin查询对象)
+    @Select("select * from adminsinfo where admin = #{admin}")
+    List<Admininfo> login(String admin);
+
+    //修改         vid = {#vid}    admin=#{admin}
+    @Update("update adminsinfo set aid=#{aid},admin=#{admin},phone=#{phone},site=#{site},position=#{position},synopsis=#{synopsis} where vid = #{vid}")
+    void upadmininfo(Admininfo admininfo);
+
+}
